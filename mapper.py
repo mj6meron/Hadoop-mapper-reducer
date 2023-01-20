252182ddd5d9c009aa3aa43c3ceeb8eb80ce86da
Meron <59652064+mj6meron@users.noreply.github.com>
2023-01-20 16:49:05 +0000

    minimized to the first 10 lines only, because the data is too big

--- mapper.py
+++ mapper.py
@@ -3,16 +3,29 @@
 import sys
 import email
 
+# counter to keep track of the number of processed lines
+counter = 0
+
 # input comes from standard input
 for line in sys.stdin:
-    # parse the email message
-    msg = email.message_from_string(line)
+    
 
-    # get the subject and body of the email
-    subject = msg.get('subject')
-    body = msg.get_payload()
+    # check if the counter is less than or equal to 10
+    if counter <= 10:
+        # parse the email message
+        msg = email.message_from_string(line)
 
-    # check if the subject is not None
-    if subject is not None:
-        # increase counters
-        print ('%s\t%s' % (subject, body))
+        # get the subject and body of the email
+        subject = msg.get('subject')
+        body = msg.get_payload()
+
+        # check if the subject is not None
+        if subject is not None:
+            # increase counters
+            print ('%s\t%s' % (subject, body))
+
+            # increment the counter
+            counter += 1
+    else:
+        # exit the loop if the counter is greater than 10
+        break
