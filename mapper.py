7771afe3f5022140f31d0ae20166b658cc3b0298
Meron <59652064+mj6meron@users.noreply.github.com>
2023-01-20 21:19:02 +0000

    update the mapper.py

--- mapper.py
+++ mapper.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python
-
 import sys
-import email
+import pandas as pd
 
-# counter to keep track of the number of processed lines
+# function to extract the last "clean" message from the email body
+def clean_email(email):
+    return ''.join([s.strip() for s in email.splitlines()][15:])
+
+# counter variable
 counter = 0
 
 # input comes from standard input
 for line in sys.stdin:
+    # parse the email message
+    emailsdf = pd.read_csv(line.strip())
+    emailsdf['email_body'] = emailsdf['message'].apply(clean_email)
+    # get the subject and body of the email
+    emailsdf['index'] = emailsdf.index
+    index = emailsdf.get('index')
+    body = emailsdf.get('email_body')
 
-    # check if the counter is less than or equal to 10
-    if counter <= 10:
-        # parse the email message
-        msg = email.message_from_string(line)
-
-        # get the subject and body of the email
-        subject = msg.get('subject')
-        body = msg.get_payload()
-
-        # check if the subject is not None
-        if (subject and body) is not None:
-            # increase counters
-            print('%s\t%s' % (subject, body))
-
-            # increment the counter
-            counter += 1
-    else:
-        # exit the loop if the counter is greater than 10
+    # check if the body is not None
+    if body is not None:
+        # increase counters
+        print('%s\t%s' % (index, body))
+        counter += 1
+    if counter == 10:
         break
