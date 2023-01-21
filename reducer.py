902dadf74809407834734d8c6f1a9ca052dc2764
Meron <59652064+mj6meron@users.noreply.github.com>
2023-01-21 01:07:30 +0000

    Edited reducer.py

--- reducer.py
+++ reducer.py
@@ -1,41 +1,19 @@
-#!/usr/bin/env python
-
 import sys
 
-current_date = None
-current_count = 0
-date = None
+current_day = None
+current_sender = None
+count = 0
 
-# initialize count dictionary
-count = {}
-
-# input comes from standard input
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-
-    # parse the input we got from mapper.py
-    date, sender, reciever = line.split('\t', 2)
-
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_date == date:
-        current_count += count
+    day, sender, reciever = line.split('\t')
+    if current_day == day:
+        count += 1
     else:
-        if current_date:
-            # write result to standard output
-            print ('%s\t%s' % (current_date, current_count))
-        current_count = count
-        current_date = date
+        if current_day:
+            print(current_day,count)
+        current_day = day
+        count = 1
 
-# do not forget to output the last word if needed!
-if current_date == date:
-    print ('%s\t%s' % (current_date, current_count))
+if current_day == day and current_sender == sender:
+    print(current_day,current_sender,count)
